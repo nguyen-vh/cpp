438cab4f468e90a5ac4e6af5d2d5f1d95f4ea9c9
nguyen-vh <137927806+nguyen-vh@users.noreply.github.com>
2023-08-09 13:22:22 +0200

    +

---
+++ SFML_Game/PCH/stdafx.hpp
@@ -0,0 +1,37 @@
+#ifndef STDAFX_HPP
+#define STDAFX_HPP
+
+#include <cassert>
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <filesystem>
+#include <iosfwd>
+#include <iostream>
+#include <locale>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <ratio>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "SFML/Audio.hpp"
+#include "SFML/Config.hpp"
+#include "SFML/GpuPreference.hpp"
+#include "SFML/Graphics.hpp"
+#include "SFML/Main.hpp"
+#include "SFML/Network.hpp"
+#include "SFML/OpenGL.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Window.hpp"
+
+#endif
